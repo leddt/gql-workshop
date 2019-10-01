ddc2ee9220439009199641c0f23fe574c9c35a30
David Thibault <david.thibault@sigmund.ca>
2019-10-01 10:12:51 -0400

    Create initial schema classes

---
+++ GqlWorkshop/Gql/Schema/Query.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GqlWorkshop.DbModel;
+using GraphQL.Conventions;
+
+namespace GqlWorkshop.Gql.Schema
+{
+    public class Query
+    {
+        public IList<QuoteGraphType> Quotes([Inject] AppDbContext db)
+        {
+            var quotes = db.Quotes.ToList();
+            return quotes.Select(q => new QuoteGraphType(q)).ToList();
+        }
+    }
+}
---
+++ GqlWorkshop/Gql/Schema/QuoteGraphType.cs
@@ -0,0 +1,19 @@
+﻿using GqlWorkshop.DbModel;
+using GraphQL.Conventions;
+
+namespace GqlWorkshop.Gql.Schema
+{
+    public class QuoteGraphType
+    {
+        private readonly Quote data;
+
+        public QuoteGraphType(Quote data)
+        {
+            this.data = data;
+        }
+
+        public Id Id => Id.New<Quote>(data.Id);
+        public string Text => data.Text;
+        public string SaidBy => data.SaidBy;
+    }
+}
